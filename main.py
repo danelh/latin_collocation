6af5f15cbdea0586e10f58c8ad4ac373115ac071
danelh <de778de@gmail.com>
2020-08-16 20:00:06 +0300

    set corpus

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
+from cltk.corpus.readers import get_corpus_reader
+from cltk.lemmatize.latin.backoff import BackoffLatinLemmatizer
+from cltk.tokenize.line import LineTokenizer
+
+def _import_corpus():
+    from cltk.corpus.utils.importer import CorpusImporter
+    corpus_importer = CorpusImporter('latin')
+    for x in corpus_importer.list_corpora:
+        print (x)
+        try:
+            corpus_importer.import_corpus(x)
+        except Exception as e:
+            # probably because expecting local
+            print (e)
+
+
+
+def parse_sentence(sen, lemmatizer, tokenizer):
+    tokens = tokenizer.tokenize(sen)
+    for t in tokens:
+        words = t.split()
+        res = lemmatizer.lemmatize(tokens)
+        print(res)
+
+
+
+tokenizer = LineTokenizer('latin')
+
+reader = get_corpus_reader(language='latin', corpus_name='latin_text_perseus')
+docs = list(reader.docs())
+reader._fileids = ['cicero__on-behalf-of-aulus-caecina__latin.json']
+sentences = list(reader.sents())
+print (len(sentences))
+untokenized_text = sentences[0]
+lemmatizer = BackoffLatinLemmatizer()
+parse_sentence(sentences[-1], lemmatizer, tokenizer)
 print ("test")
+
+
