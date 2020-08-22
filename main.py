8418cea7f06f2b55733c91b8ec4a027d005ff499
danelh <de778de@gmail.com>
2020-08-22 18:02:55 +0300

    trying to add ref.

--- main.py
+++ main.py
@@ -35,13 +35,23 @@ def parse_sentence(sen, lemmatizer, tokenizer):
 class AbstractCollectionMethod():
     def __init__(self):
         self.pairs = None
+        self.ref = None
 
     def find(self):
         self.pairs = self._get_pairs()
+        # print("Here:{}".format(self.get_name()))
+        # self.ref = self._get_ref()
+        # print("Here2:{}".format(self.get_name()))
         return self.pairs
 
     def _get_pairs(self):
         return {}
+
+    def _get_ref(self):
+        return {}
+
+    def get_name(self):
+        return "no_name"
 
 class DefaultCollectionMethod(AbstractCollectionMethod):
     def __init__(self, t_tsh=4.0, freq_tsh=0.1):
@@ -51,16 +61,33 @@ class DefaultCollectionMethod(AbstractCollectionMethod):
         self.total_groups = 0
         self.t_tsh = t_tsh
         self.freq_tsh = freq_tsh
-        self.ref = defaultdict(list)
+        self._ref = defaultdict(set)
+        self.grp_list = []
+        self.all_pairs = set()
 
     def parse_lemmas_in_group(self, grp):
         grp_dict = {x[1]: x[0] for x in grp}
-        lemmas = {x[1] for x in grp}
+        # due to space issue we should keep the entire grp,
+        # and later to run over the required paris only
+        self.grp_list.append(grp_dict)
+
+        lemmas = grp_dict.keys()
         for l in lemmas:
             self.lemma_counter[l] += 1
             for l2 in lemmas-{l}:
                 self.g_counter[l][l2] += 1
+                # frozenset to avoid duplicats. in comment (see above why)
+                #self._ref[frozenset((l,l2))].add(frozenset((grp_dict[l],grp_dict[l2])))
         self.total_groups += 1
+
+    def _get_ref(self):
+        for pair in self.all_pairs:
+            [l1, l2] = list(pair)
+            for grp in self.grp_list:
+                lemmas_set = set(grp.keys())
+                if pair <= lemmas_set:
+                    self._ref[pair].add(frozenset((grp[l1], grp[l2])))
+        return self._ref
 
     def _get_pairs(self):
 
@@ -73,6 +100,7 @@ class DefaultCollectionMethod(AbstractCollectionMethod):
                     if t > self.t_tsh:
                         # print(l, paired_l, t)
                         pairs[l][paired_l] = t
+                        self.all_pairs.add(frozenset((l, paired_l)))
         return pairs
 
     def analyze_pair(self, l1, l2, lemmas_freq_in_group):
@@ -107,7 +135,10 @@ class WordDistanceCollectionMethod(AbstractCollectionMethod):
             self.default_collection.parse_lemmas_in_group(_slice)
 
     def _get_pairs(self):
-        return self.default_collection.find()
+        return self.default_collection._get_pairs()
+
+    def _get_ref(self):
+        return self.default_collection._get_ref()
 
     def get_name(self):
         return "{}{}".format("w", str(self.word_distance))
@@ -138,6 +169,7 @@ class CollocationCollector():
             collection_method.parse_lemmas_in_group(self.lemmatize_token(token))
 
     def find_collocation(self):
+        print ("Here0", len(self.collection_methods))
         return [collection_method.find() for collection_method in self.collection_methods]
 
     def lemmatize_token(self, token):
@@ -226,11 +258,9 @@ def load_compact_nouns_json():
 
 
 x = load_compact_nouns_json()
-print (x["w1"]["litus"])
-print (x["w2"]["litus"])
-y = fix_float_for_json(x)
-# print (y["w1"]["litus"])
-# flatten_collector(x["w4"]["litus"])
+# print (x["w1"]["litus"])
+# print (x["w2"]["litus"])
+# y = fix_float_for_json(x)
 
 tokenizer = LineTokenizer('latin')
 lemmatizer = BackoffLatinLemmatizer()
@@ -241,7 +271,7 @@ docs = list(reader.docs())
 sentences = list(reader.sents())
 
 # to speedup
-sentences = sentences[::10]
+sentences = sentences[::1]
 
 print (len(sentences))
 # collection_method = DefaultCollectionMethod()
@@ -249,15 +279,17 @@ print (len(sentences))
 cm_1 = WordDistanceCollectionMethod(1, t_tsh=2, freq_tsh=0.01)
 cm_2 = WordDistanceCollectionMethod(2, t_tsh=2, freq_tsh=0.01)
 cm_4 = WordDistanceCollectionMethod(4, t_tsh=2, freq_tsh=0.01)
-cms = [cm_1, cm_2, cm_4]
+# cms = [cm_1, cm_2, cm_4]
+cms = [cm_2]
 cc = CollocationCollector(lemmatizer, None, cms)
 cc.parse(sentences)
-x = cc.find_collocation()[0]
-y = cc.find_collocation()[1]
+all_collocations = cc.find_collocation()
+x = all_collocations[0]
+# y = all_collocations[1]
 # print (cc.g_counter["capio"])
 print (x["litus"])
-print (y["litus"])
-print (y["musca"])
+# print (y["litus"])
+# print (y["musca"])
 save_collectors(cms)
 # very good examples in "stringo" "lacus" "iaceo corpus" "curnu"
 
